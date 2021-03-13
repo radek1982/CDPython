132ed69fed025a2ee5bf764e8977785961e5ec6a
Radek Wazny <radek.wazny@gmail.com>
2021-03-13 12:52:19 -0600

    Hello World

---
+++ _python/python_fundamentals/hello_world.py
@@ -0,0 +1,20 @@
+print("hello world")
+name = "Radek"
+print("Hi", name)
+print("Hi " + name)
+print(f"Hi {name}")
+
+num = 77
+
+print("Hello", num)
+
+print("Hello " + str(num))
+
+print(f"Hello {num}")
+
+f1 = "pierogi"
+f2 = "pizza"
+
+print("I like to eat {} and {} ".format(f1,f2))
+print(f"I like to eat {f1} and {f2}")
+
