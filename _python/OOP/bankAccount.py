29854df82e6d936ca13e7042f7961687820eea18
Radek Wazny <radek.wazny@gmail.com>
2021-03-13 22:01:19 -0600

    Support for multiple accounts and using BankAccount for user

--- _python/OOP/bankAccount.py
+++ _python/OOP/bankAccount.py
@@ -18,7 +18,6 @@ class BankAccount:
         return self
 
 
-
-
-account1 = BankAccount(balance=10).deposit(3).deposit(2).deposit(1).withdraw(6).yield_interest().display_account_info()
-account2 = BankAccount().deposit(100.25).deposit(50.0).withdraw(10).withdraw(10).withdraw(5).withdraw(5).yield_interest().display_account_info()
+if __name__ == "__main__":
+    account1 = BankAccount(balance=10).deposit(3).deposit(2).deposit(1).withdraw(6).yield_interest().display_account_info()
+    account2 = BankAccount().deposit(100.25).deposit(50.0).withdraw(10).withdraw(10).withdraw(5).withdraw(5).yield_interest().display_account_info()
--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -1,32 +1,40 @@
+from bankAccount import BankAccount
 class User: 
-    def __init__(self, name): 
+    def __init__(self, name):
+        self.accounts = [BankAccount(int_rate=0.02)] 
         self.name = name
-        self.balance = 0
-    def make_deposit(self, amount):
-        self.balance+=amount
+    def make_deposit(self, amount, acc=0):
+        self.accounts[acc].deposit(amount)
         return self
 
     def display_user_balance(self): 
-        print(f"{self.name} - ${self.balance:.2f}")
 
-    def make_withdrawal(self, amount): 
-        self.balance-=amount
+        for a in self.accounts: 
+            print(f"{self.name} -${a.balance:.2f}")
+
+    def make_withdrawal(self, amount, acc=0): 
+        self.accounts[acc].withdraw(amount)
         return self
 
-    def transfer_money(self, other, amount):
+    def transfer_money(self, other, amount, acc=0):
 
-        self.make_withdrawal(amount)
-        other.make_deposit(amount)
+        self.make_withdrawal(amount, acc)
+        other.make_deposit(amount,acc)
         return self
-        
+    
+    def open_account(self, account = BankAccount()):
+        self.accounts.append(account)
+        return self
 
-user1 = User("Radek")
-user2 = User("Piotrek")
-user3 = User("Benek")
+if __name__ == "__main__":
+    user1 = User("Radek")
+    user2 = User("Piotrek")
+    user3 = User("Benek")
 
-user1.make_deposit(10).make_deposit(20).make_deposit(30).make_withdrawal(20).display_user_balance()
+    user1.open_account().make_deposit(5, 1)
+    user1.make_deposit(10).make_deposit(20).make_deposit(30).make_withdrawal(20).display_user_balance()
 
-user2.make_deposit(5).make_deposit(7).make_withdrawal(1).make_withdrawal(1).display_user_balance()
+    user2.make_deposit(5).make_deposit(7).make_withdrawal(1).make_withdrawal(1).display_user_balance()
 
-user1.transfer_money(user3, 10).display_user_balance()
-user3.display_user_balance()
+    user1.transfer_money(user3, 10).display_user_balance()
+    user3.display_user_balance()
