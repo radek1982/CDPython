ac71d0f0d46ef5836f15447053eab159a0488449
Radek Wazny <radek.wazny@gmail.com>
2021-03-13 21:15:28 -0600

    method chaining

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -4,38 +4,29 @@ class User:
         self.balance = 0
     def make_deposit(self, amount):
         self.balance+=amount
+        return self
 
     def display_user_balance(self): 
         print(f"{self.name} - ${self.balance:.2f}")
 
     def make_withdrawal(self, amount): 
         self.balance-=amount
+        return self
 
     def transfer_money(self, other, amount):
 
         self.make_withdrawal(amount)
         other.make_deposit(amount)
+        return self
         
 
 user1 = User("Radek")
 user2 = User("Piotrek")
 user3 = User("Benek")
 
-user1.make_deposit(10)
-user1.make_deposit(20)
-user1.make_deposit(30)
+user1.make_deposit(10).make_deposit(20).make_deposit(30).make_withdrawal(20).display_user_balance()
 
-user1.make_withdrawal(20)
+user2.make_deposit(5).make_deposit(7).make_withdrawal(1).make_withdrawal(1).display_user_balance()
 
-user1.display_user_balance()
-
-user2.make_deposit(5)
-user2.make_deposit(7)
-user2.make_withdrawal(1)
-user2.make_withdrawal(1)
-
-user2.display_user_balance()
-
-user1.transfer_money(user3, 10)
-user1.display_user_balance()
+user1.transfer_money(user3, 10).display_user_balance()
 user3.display_user_balance()
